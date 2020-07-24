f17e1eac0dbee04776b55ee9bedb4e850e7fec32
Dimanson perez <dimaps716@gmail.com>
2020-07-24 14:55:04 -0500

    reto terminado

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 # Resolve the problem!!
+import re, os 
 
 
 def run():
+    # pongo toda la ruta del archivo porque estoy trabajando en windows si estubiera en mac solo colocaria "encode.txt"
+    #Es neserio que la ruta del archivo tenga boble barra invertida "\\" de lo comtrario dara erro "(unicode error) 'unicodeescape' "
+    with open ("C:\\Users\\MARIA ANGELICA\\Cursos_DIMA\\git_clone\\challenge-python-03\\src\\encoded.txt", 'r', encoding='utf-8') as file:
+        regex = re.compile('[a-z]')
+        mensaje = re.findall(regex, file.read())
+        print(''.join(mensaje))
     # Start coding here
 
 
